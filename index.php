8043d7354e31c73b0c8d75f7e1b54af2d2d8974b
Matias monsalve <mmonsalve15@alumnos.utalca.cl>
2019-09-04 23:10:48 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<link rel="stylesheet" href="estilos/estilos.css">
+<meta charset="utf-8">
+<title>MATÍAS MONSALVE </title>
+<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+<meta name="viewport" content="width=device-width, user-scalable=no">
+
+</head>
+
+
+<body>
+
+<a id="whatsapp" href="https://api.whatsapp.com/send?phone=56957705073"><img src="imagenes/whatsapp.png" alt=""></a>
+<header>
+  <div class="menu">
+    <div class="contenedor">
+      <p class="logo">MATÍAS MONSALVE</p>
+      <img class="menu-icon" src="imagenes/menu.png" alt="">
+      <nav>
+        <ul class="lista-menu">
+          <li><a href="#sobre-mi">Sobre mi</a></li>
+          <li><a href="#servicios">Servicios</a></li>
+          <li><a href="#mis-trabajos">Mis Trabajos</a></li>
+          <li><a href="#por-que-trabajar">¿Por que trabajar conmigo?</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+
+<div class="contenedor" id="contenedor-titulo-flex">
+  <div class="contenedor-titulo">
+    <h1>Matías Monsalve</h1>
+    <h2>Consultoria, Desarrollo web y Trading</h2>
+    <a href="">VER MIS TRABAJOS</a>
+  </div>
+</div>
+
+</header>
+
+
+<main>
+
+<section id="sobre-mi">
+  <div class="contenedor">
+    <h3>Sobre mi</h3>
+    <div class="contenedor-sobremi">
+      <div class="computadoras">
+        <img src="imagenes/computadoras.png">
+      </div>
+      <div class="texto">
+        <p>Mi nombre es Matias monsalve y soy estudiante de ingenieria en informatica empresarial. Tengo la experiencia en gestión empresarial, programación y desarrollo web junto a distintas estrategias de marketing en general.</p>
+  						<p>Me aseguro de utilizar siempre las últimas tendencias en tecnologia y buenas practicas empresariales. Si necesitás <span> POTENCIAR TU NEGOCIO </span>, te aseguro que llegaste al lugar correcto.</p>
+  						<p>Puedes ponerte en contacto conmigo a través de Facebook, Linkedin o completando el formulario de contacto que se encuentra al final de mi sitio web.</p>
+          <a href="">ver mis trabajos </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="servicios">
+  <div class="contenedor">
+    <h3>Servicios</h3>
+    <div class="contenedor-servicios">
+      <div class="servicio violeta">
+        <h4> Desarrollo web  </h4>
+
+        <ul>
+        <li>HTML5 y CSS3</li>
+
+        <li>Responsive Design</li>
+
+        <li>Efectos con la propiedad hover</li>
+
+        <li>Transformaciones de objetos con la propiedad transform</li>
+
+        <li>Animaciones avanzadas con jQuery</li>
+
+       <li> Estructurar el HTML para aumentar el Posicionamiento en los Buscadores</li>
+          </ul>
+      
+        <img class="icono" src="imagenes/monitor.png">
+        <img class="ondas" src="imagenes/waves.png">
+      </div>
+      <div class="servicio celeste">
+        <h4>Diseño y creacion de base de datos  </h4>
+      <p>Una base de datos correctamente diseñada le proporciona acceso a información actualizada y precisa. Dado que un diseño correcto es esencial para lograr los objetivos en trabajar con una base de datos </p>
+        <img class="icono" src="imagenes/mobile.png">
+        <img class="ondas" src="imagenes/waves.png">
+      </div>
+      <div class="servicio violeta">
+        <h4> Supply Chain Managament  </h4>
+       <p> responsable de gestionar y organizar todas las actividades de adquisición, producción y distribución de los bienes que
+        una compañía pone a disposición de sus clientes. Por lo tanto, es el mayor gestor de la cadena de suministro o Supply
+        Chain.</p>
+        <img class="icono" src="imagenes/seo.png">
+        <img class="ondas" src="imagenes/waves.png">
+      </div>
+      <div class="servicio celeste">
+        <h4> Trading </h4>
+
+        <p> estudio de los mercados mediante el análisis técnico y el análisis fundamental para invertir en diferentes instrumentos
+      financieros con el objetivo de obtener un beneficio</p>
+        <img class="icono" src="imagenes/wordpress.png">
+        <img class="ondas" src="imagenes/waves.png">
+      </div>
+      <div class="servicio violeta">
+       <h4> Opciones Binarias </h4>
+        <p> La opciones binarias son un sistema de inversión en los que se apuesta a la subida o bajada en el precio de activos
+        subyacentes que son cotizados en un mercado bursátil durante un tiempo determinado.
+        </p>
+        <img class="icono" src="imagenes/mante.png">
+        <img class="ondas" src="imagenes/waves.png">
+      </div>
+      <div class="servicio celeste">
+        <h4> Opciones Digitales </h4>
+        <p> Las opciones digitales incluyen características de las opciones binarias y de las clásicas, haciendo que se trate de una combinación perfecta. 
+        podemos decir que es diferente y que puede ser muy conveniente para determinados traders, según sea su estilo de trading.</p>
+        <img class="icono" src="imagenes/marketing.png">
+        <img class="ondas" src="imagenes/waves.png">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="mis-trabajos">
+  <div class="contenedor">
+    <h3>Mis Trabajos</h3>
+    <div class="owl-carousel owl-theme">
+    <div class="item">
+      <a href="www.google.com">
+      <img src="imagenes/sassy.jpg">
+      <p>Sassy Kids</p>
+      </a>
+    </div>
+    <div class="item">
+      <a href="www.google.com">
+      <img src="imagenes/sassy.jpg">
+      <p>Sassy Kids</p>
+      </a>
+    </div>
+    <div class="item">
+      <a href="www.google.com">
+      <img src="imagenes/sassy.jpg">
+      <p>Sassy Kids</p>
+      </a>
+    </div>
+    <div class="item">
+      <a href="www.google.com">
+      <img src="imagenes/sassy.jpg">
+      <p>Sassy Kids</p>
+      </a>
+    </div>
+    <div class="item">
+      <a href="www.google.com">
+      <img src="imagenes/sassy.jpg">
+      <p>Sassy Kids</p>
+      </a>
+    </div>
+    <div class="item">
+      <a href="www.google.com">
+      <img src="imagenes/sassy.jpg">
+      <p>Sassy Kids</p>
+      </a>
+    </div>
+</div>
+  </div>
+</section>
+
+<section id="por-que-trabajar">
+  <div class="contenedor">
+    <h3>¿Por que trabajar conmigo?</h3>
+    <div class="contenedor-trabajar">
+      <div class="item">
+        <ul>
+          <li><img src="imagenes/velocidad.png" alt=""></li>
+          <li>Velocidad</li>
+          <li>Me aseguro de que tu web cargue entre 2 y 4 segundos (depende de la cantidad de contenido del sitio). Si una web tarda más de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar más</li>
+        </ul>
+      </div>
+      <div class="item">
+        <ul>
+          <li><img src="imagenes/mantenimiento.png" alt=""></li>
+          <li>Mantenimiento</li>
+          <li>Mantengo una comunicación constante con mi cliente, para enseñarle a modificar contenido mediante el panel de Wordpress o para cualquier duda/cambio que quiera hacer en su sitio web</li>
+        </ul>
+      </div>
+      <div class="item">
+        <ul>
+          <li><img src="imagenes/mobile2.png" alt=""></li>
+          <li>Mobile Priority</li>
+          <li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo móvil</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="blog">
+  <div class="contenedor">
+    <h3>Blog</h3>
+    <div class="contenedor-publicaciones">
+      <div class="publicacion">
+        <img src="imagenes/blog1.jpg" alt="">
+        <div class="contenido">
+          <h4>No alcanza con tener solamente una página web</h4>
+          <p>¿Quieress aprender cuales son las herramientas que usan las mejores marcas para generar tráfico?</p>
+          <a href="">Leer mas ➝</a>
+        </div>
+      </div>
+      <div class="publicacion">
+        <img src="imagenes/blog2.jpg" alt="">
+        <div class="contenido">
+          <h4>¿Qué es el remarketing y como utilizarlo?</h4>
+          <p>El remarketing es una de las técnicas mas utlizadas por las mejores companías! ¿Querés saber de que se trata?</p>
+          <a href="">Leer mas ➝</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="que-esperas">
+  <div class="contenedor">
+    <h3>¿Que esperas para tener tu página web?</h3>
+    <a href="">HABLEMOS</a>
+  </div>
+</section>
+
+<section id="contacto">
+  <?php
+      if(@$_GET['i']=='ok') { // QUIERE DECIR QUE EL FORMULARIO SE ENVIO CORRECTAMENTE ?>
+
+      <h3>La consulta se envio correctamente. Nos contactaremos a la brevedad.</h3>
+
+      <?php
+      } else{
+      ?>
+  <div class="contenedor">
+    <h3>Contacto</h3>
+    <form action="envio-formulario.php" method="post" enctype="multipart/form-data">
+      <input type="text" placeholder="Nombre" name="nombre" required>
+      <input type="email" placeholder="Email" name="email" required>
+      <textarea placeholder="Mensaje" name="mensaje" required></textarea>
+      <input type="submit" value="ENVIAR MENSAJE">
+    </form>
+          <?php } ?>
+
+
+   <div class="contacto-info">
+      <div class="mail"><img src="imagenes/mail-icon.png" alt="icono de mail"> mmonsalve15@alumnos.utalca.cl</div>
+      <div class="whatsapp"><img src="imagenes/whatsapp-icon.png" alt=""> +56 9 57705073</div>
+    </div>
+
+
+
+  </div>
+</section>
+
+</main>
+
+<footer>
+  <div class="contenedor">
+    <div class="redes">
+      <a href="#"><img src="imagenes/facebook.png" alt="imagen de facebook"></a>
+      <a href="#"><img src="imagenes/medium.png" alt="imagen de facebook"></a>
+      <a href="#"><img src="imagenes/github.png" alt="imagen de facebook"></a>
+      <a href="#"><img src="imagenes/linkedin.png" alt="imagen de facebook"></a>
+    </div>
+    <div class="parrafo">
+      <p>2019 Matías Monsalve | Diseño web. Todos los derechos reservados</p>
+    </div>
+  </div>
+</footer>
+
+
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="owl/owl.carousel.min.js"></script>
+<script>
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+</script>
+
+<script>
+
+$(document).ready(function(){
+
+$(window).scroll(function(){
+  scroll = $(window).scrollTop();
+  if (scroll > 100) {
+    $('.menu').css({"position":"fixed"});
+    $('.menu').css({"width":"100%"});
+    $('.menu').css({"top":"0"});
+    $('.menu').css({"background":"#fff"});
+    $('.menu a').css({"color":"#000"});
+    $('.logo').css({"color":"#000"});
+    $('.menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 6px 1px 1px"});
+    $('.menu').css({"z-index":"100"});
+  } else {
+    $('.menu').css({"position":"relative"});
+    $('.menu').css({"background":"transparent"});
+    $('.menu').css({"box-shadow":"0 0 0"});
+    $('.menu a').css({"color":"#fff"});
+    $('.logo').css({"color":"#fff"});
+  }
+})
+
+$('.menu-icon').click(function(){
+  $('header nav').slideToggle();
+})
+
+})
+
+
+
+</script>
+
+
+</body>
+
+</html>
